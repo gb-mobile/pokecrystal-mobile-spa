8fce3c35ce89b67aa4bc741c40ae59834068a9fd
gb-mobile <110418063+gb-mobile@users.noreply.github.com>
2024-11-24 16:49:20 -0500

    Added location comments / Fixed HM> MO & TM > MT

--- data/mobile/prefectures_list.asm
+++ data/mobile/prefectures_list.asm
@@ -1,54 +1,54 @@
 Prefectures: ; Some names shortened to fit, check for official initials later. In this list, strings must be right-aligned.
-	db "ES-C @"
-	db "ES-VI@"
-	db "ES-AB@"
-	db "ES-A @"
-	db "ES-AL@"
-	db "ES-O @"
-	db "ES-AV@"
-	db "ES-BA@"
-	db "ES-B @"
-	db "ES-BI@"
-	db "ES-BU@"
-	db "ES-CC@"
-	db "ES-CA@"
-	db "ES-S @"
-	db "ES-CS@"
-	db "ES-CR@"
-	db "ES-CO@"
-	db "ES-CU@"
-	db "ES-SS@"
-	db "ES-GI@"
-	db "ES-GR@"
-	db "ES-GU@"
-	db "ES-H @"
-	db "ES-HU@"
-	db "ES-PM@"
-	db "ES-J @"
-	db "ES-LO@"
-	db "ES-GC@"
-	db "ES-LE@"
-	db "ES-L @"
-	db "ES-LU@"
-	db "ES-M @"
-	db "ES-MA@"
-	db "ES-MU@"
-	db "ES-NA@"
-	db "ES-OR@"
-	db "ES-P @"
-	db "ES-PO@"
-	db "ES-SA@"
-	db "ES-TF@"
-	db "ES-SG@"
-	db "ES-SE@"
-	db "ES-SO@"
-	db "ES-T @"
-	db "ES-TE@"
-	db "ES-TO@"
-	db "ES-V @"
-	db "ES-VA@"
-	db "ES-ZA@"
-	db "ES-Z @"
+	db "ES-C @"		; A Coruña
+	db "ES-VI@"		; Araba
+	db "ES-AB@"		; Albacete
+	db "ES-A @"		; Alicante
+	db "ES-AL@"		; Almería
+	db "ES-O @"		; Asturias
+	db "ES-AV@"		; Ávila
+	db "ES-BA@"		; Badajoz
+	db "ES-B @"		; Barcelona
+	db "ES-BI@"		; Bizkaia
+	db "ES-BU@"		; Burgos
+	db "ES-CC@"		; Cáceres
+	db "ES-CA@"		; Cádiz
+	db "ES-S @"		; Cantabria
+	db "ES-CS@"		; Castellón
+	db "ES-CR@"		; Ciudad Real
+	db "ES-CO@"		; Córdoba
+	db "ES-CU@"		; Cuenca
+	db "ES-SS@"		; Gipuzkoa
+	db "ES-GI@"		; Girona
+	db "ES-GR@"		; Granada
+	db "ES-GU@"		; Guadalajara
+	db "ES-H @"		; Huelva
+	db "ES-HU@"		; Huesca
+	db "ES-PM@"		; Illes Balears
+	db "ES-J @"		; Jaén
+	db "ES-LO@"		; La Rioja
+	db "ES-GC@"		; Las Palmas
+	db "ES-LE@"		; León
+	db "ES-L @"		; Lleida
+	db "ES-LU@"		; Lugo
+	db "ES-M @"		; Madrid
+	db "ES-MA@"		; Málaga
+	db "ES-MU@"		; Murcia
+	db "ES-NA@"		; Nafarroa
+	db "ES-OR@"		; Ourense
+	db "ES-P @"		; Palencia
+	db "ES-PO@"		; Pontevedra
+	db "ES-SA@"		; Salamanca
+	db "ES-TF@"		; Santa Cruz de Tenerife
+	db "ES-SG@"		; Segovia
+	db "ES-SE@"		; Sevilla
+	db "ES-SO@"		; Soria
+	db "ES-T @"		; Tarragona
+	db "ES-TE@"		; Teruel
+	db "ES-TO@"		; Toledo
+	db "ES-V @"		; Valencia
+	db "ES-VA@"		; Valladolid
+	db "ES-ZA@"		; Zamora
+	db "ES-Z @"		; Zaragoza
 	db "AD-07@"      ; Escut Canillo
 	db "AD-02@"      ; Escut Encamp
 	db "AD-03@"      ; Massana
