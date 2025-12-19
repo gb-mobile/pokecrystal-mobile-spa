40d367c39b988ed944c85e5c7591b06038b68033
gb-mobile <110418063+gb-mobile@users.noreply.github.com>
2025-12-18 16:40:12 -0800

    Added a proper 4 character word for 'QUIT' in Spanish
    Thanks Fillo

--- mobile/mobile_5c.asm
+++ mobile/mobile_5c.asm
@@ -776,9 +776,9 @@ INCBIN "gfx/pokedex/slowpoke_mobile.2bpp.lz"
 String_172e31:
 	db "Ingrese CÓDIGO@";"パスワード<WO>いれてください@"
 String_172e3f:
-	db " CAMB SAL   OK @";"きりかえ　やめる　　けってい@"
+	db " CAMB IRSE  OK @";"きりかえ　やめる　　けってい@"
 String_172e4e:
-	db " CAMB SAL   OK @";"きりかえ　やめる　　"
+	db " CAMB IRSE  OK @";"きりかえ　やめる　　"
 String_172e58:
 	db "OK@";"けってい@"
 String_172e5d:
