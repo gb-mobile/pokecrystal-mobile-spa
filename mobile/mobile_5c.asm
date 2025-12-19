6e0e7a8b35261aba853cca44cec5f2eed07486ce
gb-mobile <110418063+gb-mobile@users.noreply.github.com>
2025-12-19 13:11:01 -0800

    Standardized

--- mobile/mobile_5c.asm
+++ mobile/mobile_5c.asm
@@ -776,9 +776,9 @@ INCBIN "gfx/pokedex/slowpoke_mobile.2bpp.lz"
 String_172e31:
 	db "Ingrese CÓDIGO@";"パスワード<WO>いれてください@"
 String_172e3f:
-	db " CAMB IRSE  OK @";"きりかえ　やめる　　けってい@"
+	db " CAMB CANC  OK @";"きりかえ　やめる　　けってい@"
 String_172e4e:
-	db " CAMB IRSE  OK @";"きりかえ　やめる　　"
+	db " CAMB CANC  OK @";"きりかえ　やめる　　"
 String_172e58:
 	db "OK@";"けってい@"
 String_172e5d:
